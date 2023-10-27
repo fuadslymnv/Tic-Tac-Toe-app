665ed4567f95276b091383e1c2eba7c405e20268
fuadslymnv <fuadsuleymanov03@gmail.com>
2023-10-27 09:02:48 +0400

    Editing button is configured

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,18 @@
+import { useState } from "react";
+
 const Player = ({ name, symbol }) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const handleEditClick = (e) => {
+    setIsEditing(true);
+  };
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {!isEditing && <span className="player-name">{name}</span>}
+        {isEditing && <input type="text" placeholder={name} required />}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={handleEditClick}>Edit</button>
     </li>
   );
 };
